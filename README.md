6ef4447ef49621c3cb4fbed1f373deff95b7521f
gbadesimple <olugbadeolalekan@yahoo.com>
2020-03-06 12:28:56 +0000

    linked list project complete

---
+++ README.md
@@ -0,0 +1,9 @@
+LinkedList project from The Odin Project.
+
+A linked list is a linear collection of data elements called nodes that “point” to the next node by means of a pointer.
+
+Each node holds a single element of data and a link or pointer to the next node in the list.
+
+A head node is the first node in the list, a tail node is the last node in the list. Below is a basic representation of a linked list:
+
+[ NODE(head) ] -> [ NODE ] -> [ NODE(tail) ] -> nil
---
+++ linkedlist.rb
@@ -0,0 +1,145 @@
+class LinkedList
+  attr_reader :head
+
+  def initialize (value = nil)
+    @head = Node.new(value)
+  end
+
+  def append(value)
+    node = @head
+    node = node.next_node until node.next_node.nil?
+    node.next_node = Node.new(value)
+  end
+
+  def prepend(value)
+    @head = Node.new(value, @head)
+  end
+
+  def size
+    node = @head
+    size = 1
+    until node.next_node.nil?
+      node = node.next_node
+      size += 1
+    end
+    size
+  end
+
+  def tail
+    node = @head
+    node = node.next_node until node.next_node.nil?
+    node
+  end
+
+  def at(index)
+    node = @head
+    index.times { node = node.next_node }
+    node
+  end
+
+  def pop
+    node = @head
+    prev = nil
+    until node.next_node.nil?
+      prev = node
+      node = node.next_node
+    end
+    prev.next_node = nil
+  end
+
+  def contains?(value)
+    return true if @head.value == value
+    node = @head
+    until node.next_node.nil?
+      node = node.next_node
+      return true if node.value == value
+    end
+    false
+  end
+
+  def find(data)
+    node = @head
+    index = 0
+    until node.next_node.nil? || node.value == data
+      node = node.next_node
+      index += 1
+    end
+    node.value == data ? index : -1
+  end
+
+  def to_s
+    node = @head
+    str = "( #{node} ) -> "
+    until node.next_node.nil?
+      str += "( #{node.next_node} ) -> "
+      node = node.next_node
+    end
+    str += 'nil'
+    str
+  end
+
+  def insert_at(index, value)
+    node = @head
+    prev = nil
+    index.times do
+      prev = node
+      node = node.next_node
+    end
+    prev.next_node = Node.new(value, node)
+  end
+
+  def remove_at(index)
+    node = @head
+    prev = nil
+    index.times do
+      prev = node
+      node = node.next_node
+    end
+    prev.next_node = node.next_node
+  end
+
+  class Node
+    def initialize(value = nil, next_node = nil)
+      @value = value
+      @next_node = next_node
+    end
+    attr_accessor :value
+    attr_accessor :next_node
+
+    def to_s
+      @value
+    end
+  end
+end
+
+list = LinkedList.new('Olalekan')
+puts list
+list.append('Olugbade')
+puts list
+list.prepend('Sunday')
+puts list
+puts "list contains #{list.size} elements"
+puts "the first element is #{list.head}"
+puts "the last element is #{list.tail}"
+puts "the element at index 2 is #{list.at(2)}"
+puts 'pop for show!'
+list.pop
+puts list
+puts " "
+puts "list contains 'Olalekan' : #{list.contains? 'Olalekan'}"
+puts "list contains 'adewale' : #{list.contains? 'adewale'}"
+list.append('javascript')
+list.append('ruby')
+list.append('python')
+puts list
+puts " "
+puts "'javascript' is at index #{list.find 'javascript'}"
+puts "'ruby' is at index #{list.find 'ruby'}"
+puts "'python' is at index #{list.find 'python'}"
+puts "inserting 'good-work' at index 2"
+list.insert_at 2, 'good-work'
+puts list
+puts " "
+puts "removing at index 4"
+list.remove_at 4
+puts list
