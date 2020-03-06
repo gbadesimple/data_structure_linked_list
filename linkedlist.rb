8606f843d2dc31946c2e18b253a7c2df64118227
gbadesimple <olugbadeolalekan@yahoo.com>
2020-03-06 12:28:56 +0000

    linked list project complete

--- linkedlist.rb
+++ linkedlist.rb
@@ -1,4 +1,22 @@
 class LinkedList
+<<<<<<< HEAD
+=======
+
+  # node class initialized in the LinkedList class
+  class Node
+    def initialize(value = nil, next_node = nil)
+      @value = value
+      @next_node = next_node
+    end
+    attr_accessor :value
+    attr_accessor :next_node
+
+    def to_s
+      @value
+    end
+  end
+
+>>>>>>> 97907bf... linked list project complete
   attr_reader :head
 
   def initialize (value = nil)
@@ -98,6 +116,7 @@ class LinkedList
     prev.next_node = node.next_node
   end
 
+<<<<<<< HEAD
   class Node
     def initialize(value = nil, next_node = nil)
       @value = value
@@ -110,6 +129,8 @@ class LinkedList
       @value
     end
   end
+=======
+>>>>>>> 97907bf... linked list project complete
 end
 
 list = LinkedList.new('Olalekan')
